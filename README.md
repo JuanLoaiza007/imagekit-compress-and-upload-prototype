08d8f94484ca53aab9cabf10fbfd82e69056cc43
juanloaiza007 <juandaloaiza723@gmail.com>
2025-04-29 15:31:02 -0500

    feat: implement image upload and compression prototype

--- README.md
+++ README.md
@@ -1,36 +1,117 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Imagekit Compress & Upload Prototype
 
-## Getting Started
+This is a Next.js project that enables image uploading using ImageKit.io. It utilizes Tailwind CSS for styling and lucide-react icons for a modern and functional user interface.
 
-First, run the development server:
+This project was created following the [ImageKit.io Next.js Tutorial](https://imagekit.io/docs/integration/nextjs) and the [API Reference for File Upload](https://imagekit.io/docs/api-reference/upload-file/upload-file).
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
+## Functionality
+
+The main page allows you to:
+
+- **Select an image** from your device.
+- **View a preview** of the image before uploading.
+- **Upload the image** to ImageKit.io.
+- **Visualize the upload progress**.
+- **Obtain the URL** of the image once uploaded.
+- **Copy the URL** to the clipboard.
+- **Cancel the selection** of the current image.
+
+## Technologies Used
+
+- **Next.js:** React framework for building web applications with server-side rendering and static site generation.
+- **Tailwind CSS:** Utility-first CSS framework for rapid and flexible UI development.
+- **lucide-react:** Library of beautiful and consistent SVG icons.
+- **@imagekit/next:** ImageKit.io SDK for Next.js, facilitating integration with the image management platform.
+
+## Project Structure
+
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+.
+├── eslint.config.mjs
+├── jsconfig.json
+├── next.config.mjs
+├── package.json
+├── package-lock.json
+├── postcss.config.mjs
+├── public
+│   ├── file.svg
+│   ├── globe.svg
+│   ├── next.svg
+│   ├── vercel.svg
+│   └── window.svg
+├── README.md
+└── src
+└── app
+├── api
+│   └── upload-auth
+│       └── route.js
+├── favicon.ico
+├── globals.css
+├── layout.jsx
+└── page.jsx
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
+```
 
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
+- `package.json`: Contains the project dependencies and scripts.
+- `src/app/page.jsx`: Main component for the image upload page.
+- `src/app/api/upload-auth/route.js`: API route to fetch the necessary authentication parameters for uploading to ImageKit.io from the server.
 
-## Learn More
+## Configuration
 
-To learn more about Next.js, take a look at the following resources:
+Before running the project, ensure you configure the ImageKit.io environment variables.
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+1.  **Install dependencies:**
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
+    ```bash
+    npm install
+    # or
+    yarn install
+    # or
+    pnpm install
+    ```
 
-## Deploy on Vercel
+2.  **Configure environment variables:**
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+    Create a `.env.local` file in the root of your project (if it doesn't exist) and add your ImageKit.io keys:
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+    ```
+    IMAGEKIT_PRIVATE_KEY=YOUR_IMAGEKIT_PRIVATE_KEY
+    IMAGEKIT_PUBLIC_KEY=YOUR_IMAGEKIT_PUBLIC_KEY
+    ```
+
+    **Important:** For security reasons, the ImageKit.io private key should be kept on the server side and **never** exposed in the client-side code. The API route (`src/app/api/upload-auth/route.js`) handles fetching the necessary authentication parameters securely.
+
+## Running the Project
+
+1.  **Run in development mode:**
+
+    ```bash
+    npm run dev
+    # or
+    yarn dev
+    # or
+    pnpm dev
+    ```
+
+    Open [http://localhost:3000](http://localhost:3000) in your browser to view the application.
+
+2.  **Build the application for production:**
+
+    ```bash
+    npm run build
+    # or
+    yarn build
+    # or
+    pnpm build
+    ```
+
+3.  **Start the application in production mode:**
+
+    ```bash
+    npm run start
+    # or
+    yarn start
+    # or
+    pnpm start
+    ```
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,175 @@
-export default function Page() {
-  return <div>Welcome to Next.js!</div>;
+"use client";
+import { upload } from "@imagekit/next";
+import { useState, useRef } from "react";
+import { Check, Copy, Trash2 } from "lucide-react";
+
+export default function UploadFilePage() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [previewUrl, setPreviewUrl] = useState(null);
+  const fileInputRef = useRef(null);
+  const [progress, setProgress] = useState(0);
+  const [uploadUrl, setUploadUrl] = useState(null);
+  const [copySuccess, setCopySuccess] = useState(false);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file && file.type.startsWith("image/")) {
+      setSelectedFile(file);
+      setPreviewUrl(URL.createObjectURL(file));
+      setUploadUrl(null);
+    } else if (file) {
+      alert("Please select only image files.");
+      event.target.value = null;
+      setSelectedFile(null);
+      setPreviewUrl(null);
+      setUploadUrl(null);
+    }
+  };
+
+  const handleButtonSelectFile = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleDiscardImage = () => {
+    setSelectedFile(null);
+    setPreviewUrl(null);
+    if (fileInputRef.current) {
+      fileInputRef.current.value = null;
+    }
+  };
+
+  const handleUploadFile = async () => {
+    if (!selectedFile) return;
+
+    try {
+      const authResponse = await fetch("/api/upload-auth");
+      if (!authResponse.ok)
+        throw new Error("Error getting authentication parameters");
+      const { token, signature, expire, publicKey } = await authResponse.json();
+
+      const transformationParams = {
+        pre: "q-60,f-webp",
+      };
+
+      const uploadResponse = await upload({
+        file: selectedFile,
+        fileName: selectedFile.name,
+        publicKey,
+        token,
+        transformation: transformationParams,
+        signature,
+        expire,
+        onProgress: (event) =>
+          setProgress(Math.round((event.loaded / event.total) * 100)),
+      });
+
+      setUploadUrl(uploadResponse.url);
+      setSelectedFile(null);
+      setPreviewUrl(null);
+      setProgress(0);
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
+
+  const handleCopyToClipboard = async () => {
+    if (uploadUrl) {
+      try {
+        await navigator.clipboard.writeText(uploadUrl);
+        setCopySuccess(true);
+        setTimeout(() => setCopySuccess(false), 2000);
+      } catch (err) {
+        console.error("Error copying to clipboard:", err);
+      }
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-md shadow-md w-full max-w-md">
+        <h2 className="text-2xl font-semibold mb-4 text-gray-800 text-center">
+          Imagekit Compress & Upload Prototype
+        </h2>
+        <div className="flex flex-col space-y-4">
+          <div className="flex items-center space-x-4">
+            {previewUrl && (
+              <div className="w-32 h-32 rounded-md overflow-hidden">
+                <img
+                  src={previewUrl}
+                  alt="Preview"
+                  className="object-cover w-full h-full"
+                />
+              </div>
+            )}
+            <div className="flex flex-col flex-1 space-y-2">
+              <button
+                disabled={progress > 0}
+                className={`text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${
+                  progress > 0
+                    ? "bg-gray-700 cursor-not-allowed"
+                    : "bg-blue-500 hover:bg-blue-700"
+                }`}
+                onClick={handleButtonSelectFile}
+              >
+                Select image
+              </button>
+              <input
+                type="file"
+                accept="image/*"
+                className="hidden"
+                ref={fileInputRef}
+                onChange={handleFileChange}
+              />
+              {selectedFile && (
+                <button
+                  disabled={progress > 0}
+                  className={`text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${
+                    progress > 0
+                      ? "bg-gray-700 cursor-not-allowed"
+                      : "bg-red-500 hover:bg-red-700"
+                  }`}
+                  onClick={handleDiscardImage}
+                >
+                  <Trash2 size={20} className="mr-2 inline-block" /> Discard
+                </button>
+              )}
+            </div>
+          </div>
+          <button
+            disabled={progress > 0}
+            className={`text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${
+              progress > 0
+                ? "bg-gray-700 cursor-not-allowed"
+                : "bg-green-500 hover:bg-green-700"
+            }`}
+            onClick={handleUploadFile}
+          >
+            Upload image
+          </button>
+          {progress > 0 && <p>Upload progress: {progress}%</p>}
+          {uploadUrl && (
+            <div className="mt-4 flex flex-col space-y-2">
+              <p className="text-lg text-gray-700">
+                Image uploaded successfully. Here is the link:
+              </p>
+              <div className="flex items-center space-x-2">
+                <input
+                  type="text"
+                  value={uploadUrl}
+                  readOnly
+                  className="flex-1 border rounded-md py-2 px-3 text-gray-700 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                />
+                <button
+                  onClick={handleCopyToClipboard}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                >
+                  {copySuccess ? <Check size={22} /> : <Copy size={22} />}
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
